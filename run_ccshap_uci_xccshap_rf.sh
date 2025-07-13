72b9fdf50eb152d72a92830e42bc7a514b62aa7b
rupensa <rpensa@gmail.com>
2025-07-13 10:50:18 +0200

    fix

--- run_ccshap_uci_xccshap_rf.sh
+++ run_ccshap_uci_xccshap_rf.sh
@@ -6,6 +6,6 @@ mkdir ./outuci_xccshap_rf
 while read p;
 do
   echo Processing $p;
-  timeoute 1d python ./src/expe_uci_classification_xccshap.py --id=$p --modelpath rf_model --out outuci_xccshap_rf --classifier=rf;
+  timeout 1d python ./src/expe_uci_classification_xccshap.py --id=$p --modelpath rf_model --out outuci_xccshap_rf --classifier=rf;
 done <datasets_xgb_all.txt
 
