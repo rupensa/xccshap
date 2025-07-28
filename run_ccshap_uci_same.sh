bcaec431629e448909ab9441dc63b637b75a5975
rupensa <rpensa@gmail.com>
2025-07-28 14:11:05 +0200

    fix

--- run_ccshap_uci_same.sh
+++ run_ccshap_uci_same.sh
@@ -5,6 +5,6 @@ mkdir ./outuci_same
 while read p;
 do
   echo Processing $p;
-  timeout 1d python ./src/expe_uci_classification_same.py --id=$p --modelpath rf_model --out outuci_same --classifier=rf;
+  timeout 2d python ./src/expe_uci_classification_same.py --id=$p --modelpath rf_model --out outuci_same --classifier=rf;
 done <datasets_xgb_all.txt
 
